4fd25ed21e9a88f782a677d6cc56b9ebb2d86da1
Lucas <lucaspereiravargas@universo.univates.br>
2023-04-15 19:30:15 -0300

    Feat: adiciona contorno e configurações do jogo.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -3,6 +3,7 @@ package game;
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Game extends JFrame implements Runnable {
 
@@ -50,24 +51,63 @@ public class Game extends JFrame implements Runnable {
 
     @Override
     public void paint(Graphics gr) {
-        gr.clearRect(0, 0, getWidth(), getHeight());
-        gr.setColor(new Color(r, g, b));
-        gr.drawRect(50 + x, 50 + y, 50, 50);
+        int contornoX = 50;
+        int contornoY = 50;
+        int contornoWidth = getWidth() - 100;
+        int contornoHeight = getHeight() - 100;
 
-        for (int i = 0; i < 10; i++) {
-            gr.setColor(new Color(i * 2, i * 3, i * 4));
-            gr.drawOval(vetX[i], vetY[i], 5, 5);
+        gr.clearRect(0, 0, getWidth(), getHeight());
+
+        // Desenha o quadrado maior (contorno)
+        gr.setColor(Color.BLACK); // define a cor do contorno do quadrado como preto
+        gr.drawRect(contornoX, contornoY, contornoWidth, contornoHeight); // desenha o quadrado na tela
+
+        // Desenha o quadrado menor (movido pelo teclado)
+        gr.setColor(new Color(r, g, b));
+        gr.fillRect(contornoX + x, contornoY + y, 50, 50);
+
+        for (int i = 0; i < 3; i++) {
+            gr.setColor(Color.RED);
+            gr.drawOval(vetX[i], vetY[i], 10, 10);
         }
 
         if (Math.random() > 0.9) {
-
-            tendenciaX = 2;
-            tendenciaY = 2;
+            tendenciaX = 3;
+            tendenciaY = 3;
         } else {
-            tendenciaX = 0;
-            tendenciaY = 0;
+            tendenciaX = -2;
+            tendenciaY = -2;
         }
-    }
+
+        for (int i = 0; i < 3; i++) {
+            if (verificarColisao(contornoX + x, contornoY + y, 50, 50, vetX[i], vetY[i], 10, 10)) {
+                JOptionPane.showMessageDialog(null, "Colisão com a bolinha " + i + "! Fim de jogo!");
+                System.exit(0);
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int novaPosX = vetX[i] + (int) (Math.random() * 5) - 2 + tendenciaX;
+            int novaPosY = vetY[i] + (int) (Math.random() * 5) - 2 + tendenciaY;
+
+            // Verifica se a bola ultrapassa a borda esquerda ou direita do contorno
+            if (novaPosX < contornoX) {
+                novaPosX = contornoX;
+            } else if (novaPosX > contornoX + contornoWidth) {
+                novaPosX = contornoX + contornoWidth;
+            }
+
+            // Verifica se a bola ultrapassa a borda superior ou inferior do contorno
+            if (novaPosY < contornoY) {
+                novaPosY = contornoY;
+            } else if (novaPosY > contornoY + contornoHeight) {
+                novaPosY = contornoY + contornoHeight;
+            }
+
+            vetX[i] = novaPosX;
+            vetY[i] = novaPosY;
+        }
+     }
 
     private void formKeyPressed(java.awt.event.KeyEvent evt) {                                
         if (evt.getKeyCode() == evt.VK_UP) {
@@ -103,7 +143,6 @@ public class Game extends JFrame implements Runnable {
         if (evt.getKeyCode() == evt.VK_N) {
             b = b - 4;
         }
-
     }                               
 
     @Override
@@ -121,6 +160,11 @@ public class Game extends JFrame implements Runnable {
             }
 
         }
+    }
+
+    private boolean verificarColisao(int x1, int y1, int largura1, int altura1, int x2, int y2, int largura2, int altura2) {
+        // Verifica se as caixas delimitadoras se interceptam em pelo menos um ponto
+        return x1 < x2 + largura2 && x1 + largura1 > x2 && y1 < y2 + altura2 && y1 + altura1 > y2;
     }
 
     private Color Color(int r, Graphics g, int b) {
